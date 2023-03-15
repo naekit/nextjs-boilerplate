1689da517786d12d242fc3ba448022da96bb58f8
naekit <61828663+naekit@users.noreply.github.com>
2023-03-15 12:51:38 -0400

    changed head tags

--- pages/map.js
+++ pages/map.js
@@ -6,7 +6,7 @@ function MyPage() {
     <>
       <Head>
         <title>Map Cluster</title>
-        <meta name="description" content="Map cluster visualization of Real Estate data" />
+        <meta name="description" content="Map cluster visualization of Real Estate Data" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
