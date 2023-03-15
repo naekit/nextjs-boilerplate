40ef93924068bf0c6dfd55d8adaa8886747c499e
naekit <61828663+naekit@users.noreply.github.com>
2023-03-15 12:36:18 -0400

    added head tags for descriptions

--- pages/map.js
+++ pages/map.js
@@ -1,11 +1,23 @@
+import { Inter } from 'next/font/google'
+import Table from '@/components/Table'
+import fs from 'fs'
+import path from 'path'
 import dynamic from 'next/dynamic';
 const Map = dynamic(() => import('../components/MapFile'), { ssr: false });
 
 function MyPage() {
   return (
-    <main className='h-screen'>
-      <Map />
-    </main>
+    <>
+      <Head>
+        <title>Map Cluster</title>
+        <meta name="description" content="Map cluster visualization of Real Estate data" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className='h-screen'>
+        <Map />
+      </main>
+    </>
   );
 }
 
