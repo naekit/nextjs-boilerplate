51db9ac1584d638ada324b90951e94587e9f2e17
naekit <61828663+naekit@users.noreply.github.com>
2023-03-16 09:01:32 -0400

    tried adding proxy, ended up sticking to a link

--- setupProxy.js
+++ setupProxy.js
@@ -7,10 +7,10 @@
 //       target: 'https://drive.google.com',
 //       changeOrigin: true,
 //       onProxyReq: function(proxyReq, req, res) {
-//         proxyReq.setHeader('Origin', 'http://localhost:3000'); // Replace with your own domain
+//         proxyReq.setHeader('Origin', 'http://localhost:3000'); 
 //       },
 //       pathRewrite: {
-//         '^/proxy': '/uc', // Use /uc instead of /file/d in the URL
+//         '^/proxy': '/uc', 
 //       },
 //     })
 //   );
